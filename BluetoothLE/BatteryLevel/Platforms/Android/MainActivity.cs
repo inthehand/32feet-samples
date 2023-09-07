55222af9eedc060c7f5aadbc6a19d9f65aa2753a
Peter Freeman Foot <peter@inthehand.com>
2023-09-07 10:46:04 +0100

    permissions and runtime request updated for Android

--- BluetoothLE/BatteryLevel/Platforms/Android/MainActivity.cs
+++ BluetoothLE/BatteryLevel/Platforms/Android/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
@@ -7,4 +8,11 @@ namespace BatteryLevel;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        InTheHand.AndroidActivity.CurrentActivity = this;
+
+        RequestPermissions(new string[] { Manifest.Permission.BluetoothConnect }, 1);
+    }
 }
